ebf716c856684f146b805347c2d8e1f3a9616b0a
dima zhuk <72468555+id1mazzz@users.noreply.github.com>
2023-03-29 21:51:20 +0300

    writing comments

---
+++ homework1/Library/GetStringSquare.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library;
+
+
+namespace Library
+{
+    /// <summary>
+    /// Формирование определенных последовательностей символов.
+    /// </summary>
+    public class GetStringSquare
+    {
+        /// <summary>
+        /// Формирование и возврат строки от 1 до заданного числа.
+        /// </summary>
+        /// <param name="n">Количество чисел в последовательности.</param>
+        public void GetSequence()
+        {
+            Console.Write("Введите число N: ");
+            int n = int.Parse(Console.ReadLine());
+            string result = string.Join(", ", Enumerable.Range(1, n));
+            Console.WriteLine(result);
+        }
+
+        /// <summary>
+        /// Вывод на экран квадрата из # с указанным количеством строк.
+        /// </summary>
+        /// <param name="N">Высота квадрата (количество строк). </param>
+        public void ShowSquare()
+        {
+            Console.Write("Введите нечетное число N: ");
+            int N = int.Parse(Console.ReadLine());
+            if (N % 2 == 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(N), "N должно быть нечетным");
+                
+            }
+
+            double enter = N / 2 + 1;
+
+            for (int row = 1; row <= N; row++)
+            {
+                for (int column = 1; column <= N; column++)
+                {
+                    if (row == enter && column == enter)
+                    {
+                        Console.Write(" ");
+                    }
+
+                    else
+                    {
+                        Console.Write("#");
+                    }
+
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ homework1/homework1/ConsoleProgram.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using homework1;
+using Library;
+
+
+namespace homework1
+{
+    class ConsoleProgram
+    {
+        static void Main(string[] args)
+        {
+            GetStringSquare librarysClass = new GetStringSquare();
+            int nomer;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Введите номер задания:");
+                Console.WriteLine("1. Строка вида 1,2,3...N");
+                Console.WriteLine("2. Квадрат из #");
+                if (int.TryParse(Console.ReadLine(), out nomer))
+                {
+                    switch (nomer)
+                    {
+                        case 1:
+                            Console.Clear();
+                            Console.WriteLine("1. Строка вида 1,2,3...N");
+                            librarysClass.GetSequence();
+                            Console.ReadKey();
+                            break;
+                        case 2:
+                            Console.Clear();
+                            Console.WriteLine("2. Квадрат из #");
+                            librarysClass.ShowSquare();
+                            Console.ReadKey();
+                            break;
+                        default:
+                            Console.WriteLine("Неверный номер!");
+                            Console.ReadKey();
+                            break;
+                    }
+                }
+            }
+            while (nomer != 0);
+        }
+    }
+}
